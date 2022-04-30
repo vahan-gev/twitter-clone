e80ca00f1aaf3c21b5780401dc4025e1cfea4161
Vahan Gevorgyan <vahangevorgyan0212@gmail.com>
2022-04-30 16:23:20 +0400

    Minor Changes

--- components/Login.js
+++ components/Login.js
@@ -19,15 +19,19 @@ function Login({ providers }) {
       <div>
         {Object.values(providers).map((provider) => (
           <div key={provider.name}>
-            <button
-              className="group relative inline-flex items-center justify-start overflow-hidden rounded bg-white px-6 py-3 font-medium transition-all hover:bg-white"
+            <a
               onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+              className="group relative inline-flex cursor-pointer items-center justify-center overflow-hidden rounded-md px-6 py-3 font-bold text-white shadow-2xl"
             >
-              <span className="absolute bottom-0 left-0 mb-9 ml-9 h-48 w-48 -translate-x-full translate-y-full rotate-[-40deg] rounded bg-[#1d9bf0] transition-all duration-500 ease-out group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-              <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
-                Sign in with {provider.name}
-              </span>
-            </button>
+              <span className="absolute inset-0 h-full w-full bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 opacity-0 transition duration-300 ease-out group-hover:opacity-100"></span>
+              <span className="absolute top-0 left-0 h-1/3 w-full bg-gradient-to-b from-white to-transparent opacity-5"></span>
+              <span className="absolute bottom-0 left-0 h-1/3 w-full bg-gradient-to-t from-white to-transparent opacity-5"></span>
+              <span className="absolute bottom-0 left-0 h-full w-4 bg-gradient-to-r from-white to-transparent opacity-5"></span>
+              <span className="absolute bottom-0 right-0 h-full w-4 bg-gradient-to-l from-white to-transparent opacity-5"></span>
+              <span className="absolute inset-0 h-full w-full rounded-md border border-white opacity-10"></span>
+              <span className="absolute h-0 w-0 rounded-full bg-white opacity-5 transition-all duration-300 ease-out group-hover:h-56 group-hover:w-56"></span>
+              <span className="relative">Sign in with {provider.name}</span>
+            </a>
           </div>
         ))}
       </div>
