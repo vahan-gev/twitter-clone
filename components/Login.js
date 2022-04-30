555657e86fb64d479761fcb5be81986a4f35bd48
Vahan Gevorgyan <vahangevorgyan0212@gmail.com>
2022-04-30 16:21:27 +0400

    Minor Changes

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,14 @@
 import { signIn } from 'next-auth/react'
+import Head from 'next/head'
 import Image from 'next/image'
 
 function Login({ providers }) {
   return (
     <div className="flex flex-col items-center space-y-20 pt-48">
+      <Head>
+        <title>Login / Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Image
         src="https://rb.gy/ogau5a"
         width={150}
